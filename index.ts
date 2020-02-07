08207d822843ebfc9d6ec70e66d058395b067d3b
Alexandr Ulybin <Alexandr Ulybin>
2020-02-07 12:11:30 +0100

    7.2.2020

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ app.get('/wsdl', (req, res) => {
 app.all('/', (req, res) => {
   res.set('content-type', 'text/xml');
 
+  // envelope created by legacy system
   const envelope =
     '<?xml version="1.0" encoding="UTF-8"?>' +
     '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ns="urn:SoapServer">' +
@@ -21,6 +22,7 @@ app.all('/', (req, res) => {
     '</soap:Body>' +
     '</soap:Envelope>';
 
+  // envelope created according to the soap tutorial
   const envelope2 =
     '<?xml version="1.0" encoding="utf-8"?>' +
     '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"  xmlns:tns="urn:SoapServer">' +
@@ -31,8 +33,8 @@ app.all('/', (req, res) => {
     '</soap:Body>' +
     '</soap:Envelope>';
 
-    res.send(envelope);
-    //res.send(envelope2);
+  res.send(envelope);
+  //res.send(envelope2);
 });
 
 app.listen(8000, () => {
